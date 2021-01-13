f0ea48b914d1602f4977583309f82ff3e33edee6
Nick Gundobin <ngundobin@gmail.com>
2021-01-13 17:14:24 -0500

    finally done with tests :)))

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,85 +1,88 @@
-const db = require("../db");
-const express = require("express");
-const ExpressError = require("../expressError");
+const db = require('../db');
+const express = require('express');
+const ExpressError = require('../expressError');
 
 const invoicesRouter = new express.Router();
 
-invoicesRouter.get("/", async (req, res, next) => {
-	try {
-		const invoices = await db.query(`SELECT * FROM invoices`);
-		return res.json(invoices.rows);
-	} catch (err) {
-		next(err);
-	}
+invoicesRouter.get('/', async (req, res, next) => {
+  try {
+    const invoices = await db.query(`SELECT * FROM invoices`);
+    return res.json(invoices.rows);
+  } catch (err) {
+    next(err);
+  }
 });
 
-invoicesRouter.get("/:id", async (req, res, next) => {
-	try {
-		const result = await db.query(`SELECT * FROM invoices WHERE id=$1`, [
-			req.params.id,
-		]);
-		if (result.rows.length === 0) {
-			throw new ExpressError(
-				`Cannot find any invoice with id ${req.params.id}`,
-				404
-			);
-		}
-		return res.status(200).json(result.rows[0]);
-	} catch (err) {
-		return next(err);
-	}
+invoicesRouter.get('/:id', async (req, res, next) => {
+  try {
+    const result = await db.query(`SELECT * FROM invoices WHERE id=$1`, [
+      req.params.id,
+    ]);
+    if (result.rows.length === 0) {
+      throw new ExpressError(
+        `Cannot find any invoice with id ${req.params.id}`,
+        404
+      );
+    }
+    return res.status(200).json(result.rows[0]);
+  } catch (err) {
+    return next(err);
+  }
 });
 
-invoicesRouter.post("/", async (req, res, next) => {
-	try {
-		const addDate = new Date().toISOString();
-		const { comp_code, amt, paid } = req.body;
+invoicesRouter.post('/', async (req, res, next) => {
+  try {
+    //const addDate = new Date().toISOString();
+    const { comp_code, amt, paid, add_date, paid_date } = req.body;
 
-		const invoice = await db.query(
-			`INSERT INTO invoices (comp_code,amt,paid,add_date,paid_date) VALUES($1,$2,$3,$4,$5) RETURNING comp_code,amt,paid,add_date,paid_date`,
-			[comp_code, amt, paid, addDate, addDate]
-		);
-		return res.status(201).json(invoice.rows[0]);
-	} catch (err) {
-		return next(err);
-	}
+    const invoice = await db.query(
+      `INSERT INTO invoices (comp_code,amt,paid,add_date,paid_date) VALUES($1,$2,$3,$4,$5) RETURNING comp_code,amt,paid,add_date,paid_date`,
+      [comp_code, amt, paid, add_date, paid_date]
+    );
+    console.log(`Inside invoices: ${invoice.rows}`);
+    return res.status(201).json(invoice.rows[0]);
+  } catch (err) {
+    return next(err);
+  }
 });
 
-invoicesRouter.patch("/:id", async (req, res, next) => {
-	try {
-		const { comp_code, amt, paid, add_date, paid_date } = req.body;
-		console.log(req.body);
-		const invoice = await db.query(
-			`UPDATE invoices SET comp_code=$1, amt=$2, paid=$3, add_date=$4, paid_date=$5 WHERE id = $6 RETURNING id,comp_code,amt,paid,add_date,paid_date`,
-			[comp_code, amt, paid, add_date, paid_date, req.params.id]
-		);
-		if (result.rows.length === 0) {
-			throw new ExpressError(
-				`Cannot update invoice with id ${req.params.id}`,
-				404
-			);
-		}
-		return res.status(200).json(invoice.rows[0]);
-	} catch (err) {
-		return next(err);
-	}
+invoicesRouter.patch('/:id', async (req, res, next) => {
+  try {
+    const { comp_code, amt, paid, add_date, paid_date } = req.body;
+    console.log(req.params.id);
+    console.log(req.body);
+    const invoice = await db.query(
+      `UPDATE invoices SET comp_code=$1, amt=$2, paid=$3, add_date=$4, paid_date=$5 WHERE id = $6 RETURNING id,comp_code,amt,paid,add_date,paid_date`,
+      [comp_code, amt, paid, add_date, paid_date, req.params.id]
+    );
+    console.log(invoice.rows.length);
+    if (invoice.rows.length === 0) {
+      throw new ExpressError(
+        `Cannot update invoice with id ${req.params.id}`,
+        404
+      );
+    }
+    return res.status(200).json(invoice.rows[0]);
+  } catch (err) {
+    return next(err);
+  }
 });
 
-invoicesRouter.delete("/:id", async (req, res, next) => {
-	try {
-		const invoice = db.query(`DELETE FROM invoices WHERE id=$1`, [
-			req.params.id,
-		]);
-		if (invoice.rows.length === 0) {
-			throw new ExpressError(
-				`Cannot delete invoice with id ${req.params.id}`,
-				404
-			);
-		}
-		return res.json({ message: "DELETED" });
-	} catch (err) {
-		return next(err);
-	}
+invoicesRouter.delete('/:id', async (req, res, next) => {
+  try {
+    const invoice = db.query(`DELETE FROM invoices WHERE id=$1`, [
+      req.params.id,
+    ]);
+    if (invoice.rows) {
+      throw new ExpressError(
+        `Cannot find invoice with id ${req.params.id}`,
+        404
+      );
+    }
+    return res.status(200).json({ message: 'DELETED...' });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 module.exports = invoicesRouter;
--- routes/invoices.test.js
+++ routes/invoices.test.js
@@ -14,7 +14,9 @@ beforeEach(async function () {
   let result = await db.query(
     `INSERT INTO invoices (comp_code,amt,paid,add_date,paid_date) VALUES ('windows',1000,true,'2021-02-02T05:00:00.000Z','2021-02-02T05:00:00.000Z') RETURNING id,comp_code,amt,paid,add_date,paid_date`
   );
-  testInvoice = result.rows[0];
+  testInvoice = result.rows;
+  testInvoice[0].add_date = '2021-02-02T05:00:00.000Z';
+  testInvoice[0].paid_date = '2021-02-02T05:00:00.000Z';
 });
 
 afterEach(async () => {
@@ -30,11 +32,10 @@ afterAll(async () => {
 describe('GET /invoices', function () {
   test('Gets a list of invoices', async function () {
     const response = await request(app).get(`/invoices`);
-    delete response.body[0].id;
-    console.log(response.body);
     expect(response.statusCode).toEqual(200);
     expect(response.body).toEqual([
       {
+        id: expect.any(Number),
         comp_code: 'windows',
         amt: 1000,
         paid: true,
@@ -47,64 +48,69 @@ describe('GET /invoices', function () {
 
 describe('GET /invoices/:id', function () {
   test('Gets a invoice', async function () {
-    console.log(testInvoice);
-    const response = await request(app).get(`/invoices/${testInvoice.id}`);
-    console.log(response.body);
+    const response = await request(app).get(`/invoices/${testInvoice[0].id}`);
     expect(response.statusCode).toEqual(200);
-    expect(response.body).toEqual(testInvoice);
+    expect(response.body).toEqual(testInvoice[0]);
   });
 });
 
-// describe("POST /invoices", function () {
-// 	test("Create a invoice", async function () {
-// 		const response = await request(app).post(`/invoices`).send({
-// 			comp_code: "apple",
-// 			amt: 1000,
-// 			paid: true,
-// 			add_date: "02/02/2021",
-// 			paid_date: "02/02/2021",
-// 		});
-// 		expect(response.statusCode).toEqual(201);
-// 		expect(response.body).toEqual({
-// 			comp_code: "apple",
-// 			amt: 1000,
-// 			paid: true,
-// 			add_date: "02/02/2021",
-// 			paid_date: "02/02/2021",
-// 		});
-// 	});
-// });
+describe('POST /invoices', function () {
+  test('Create a invoice', async function () {
+    const response = await request(app).post(`/invoices`).send({
+      comp_code: 'windows',
+      amt: 1000,
+      paid: true,
+      add_date: '2021-02-02T05:00:00.000Z',
+      paid_date: '2021-02-02T05:00:00.000Z',
+    });
+    expect(response.statusCode).toEqual(201);
+    expect(response.body).toEqual({
+      comp_code: 'windows',
+      amt: 1000,
+      paid: true,
+      add_date: '2021-02-02T05:00:00.000Z',
+      paid_date: '2021-02-02T05:00:00.000Z',
+    });
+  });
+});
 
-// describe("PATCH /invoices/:code", function () {
-// 	test("Update invoice's fields", async function () {
-// 		const response = await request(app)
-// 			.patch(`/invoices/${testInvoice.id}`)
-// 			.send({
-// 				comp_code: "apple",
-// 				amt: 1200,
-// 				paid: false,
-// 				add_date: "02/02/2021",
-// 				paid_date: "02/02/2021",
-// 			});
-// 		expect(response.statusCode).toEqual(200);
-// 		expect(response.body).toEqual({
-// 			comp_code: "apple",
-// 			amt: 1200,
-// 			paid: false,
-// 			add_date: "02/02/2021",
-// 			paid_date: "02/02/2021",
-// 		});
-// 	});
-// 	test("Responds with 404 code Not Found", async function () {
-// 		const response = await request(app).patch(`/invoices/0`);
-// 		expect(response.statusCode).toEqual(404);
-// 	});
-// });
+describe('PATCH /invoices/:id', function () {
+  test("Update invoice's fields", async function () {
+    const response = await request(app)
+      .patch(`/invoices/${testInvoice[0].id}`)
+      .send({
+        comp_code: 'windows',
+        amt: 1200,
+        paid: false,
+        add_date: '02/02/2021',
+        paid_date: '02/02/2021',
+      });
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      id: expect.any(Number),
+      comp_code: 'windows',
+      amt: 1200,
+      paid: false,
+      add_date: '2021-02-02T05:00:00.000Z',
+      paid_date: '2021-02-02T05:00:00.000Z',
+    });
+  });
+  test('Responds with 404 code Not Found', async function () {
+    const response = await request(app).patch(`/invoices/0`);
+    expect(response.statusCode).toEqual(404);
+  });
+});
 
-// describe("DELETE /invoices/:id", function () {
-// 	test("Delete invoice", async function () {
-// 		const response = await request(app).delete(`/invoices/${testInvoice.id}`);
-// 		expect(response.statusCode).toEqual(200);
-// 		expect(response.body).toEqual({ message: "DELETED..." });
-// 	});
-// });
+describe('DELETE /invoices/:id', function () {
+  test('Delete invoice', async function () {
+    const response = await request(app).delete(
+      `/invoices/${testInvoice[0].id}`
+    );
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({ message: 'DELETED...' });
+  });
+  test('Responds with 404 code Not Found', async function () {
+    const response = await request(app).patch(`/invoices/0`);
+    expect(response.statusCode).toEqual(404);
+  });
+});
